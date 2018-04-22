ae6b6c0ef9510209b07cfc81cb6aae402b84e8cd
Keith Hon's Macbook Pro <klausehon@gmail.com>
2018-04-22 23:02:14 +0800

    bumped ver

--- src/pages/profile/profile.module.ts
+++ src/pages/profile/profile.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { ProfilePage } from './profile';
+// import { NgModule } from '@angular/core';
+// import { IonicPageModule } from 'ionic-angular';
+// import { ProfilePage } from './profile';
 
-@NgModule({
-  declarations: [
-    ProfilePage,
-  ],
-  imports: [
-    IonicPageModule.forChild(ProfilePage),
-  ],
-})
-export class ProfilePageModule {}
+// @NgModule({
+//   declarations: [
+//     ProfilePage,
+//   ],
+//   imports: [
+//     IonicPageModule.forChild(ProfilePage),
+//   ],
+// })
+// export class ProfilePageModule {}
